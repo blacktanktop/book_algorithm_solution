87f8fc497cdd707a22ef19d8a533e4b96eb7e2c9
blacktanktop <blacktanktopme@gmail.com>
2020-10-18 19:02:02 +0900

    出力周り変更

--- solutions/python/chap5/solution_5_2.py
+++ solutions/python/chap5/solution_5_2.py
@@ -1,6 +1,7 @@
 def main():
     n, w = map(int, input().split())
     a = [int(x) for x in input().split()]
+    print('a:', a)
     # dp[i+1][j]はi番目(indexにおいて)までの整数の中からいくつか選んで総和jにできるかという意味
     # dp[3][2]ならaの2番目(indexにおいて)までの整数の中からいくつか選んで合計が2になるなら1とする
     # dpの枠の用意(indexとn, wを合わせるので+1する)
@@ -10,16 +11,20 @@ def main():
     # ２重のfor文でO(nw)
     for i in range(n):
         for j in range(w+1):
+            # a[i]を選ばない場合
             if dp[i][j]:
                 dp[i+1][j] = dp[i][j]
-                print(i+1, '個目までの整数のいくつかで', j, 'が作れるか')
+                print(a[:i+1], 'の中の整数のいくつかで', j, 'が作れるか')
                 print(dp)
+            # a[i]を選ぶ場合
             if j >= a[i] and dp[i][j-a[i]]:
                     # a[i]を選んで成立するには
                     # dp[i][j-a[i]]がTrueである必要がある
                     dp[i+1][j] = dp[i][j-a[i]]
-                    print(i+1, '個目までの整数のいくつかで', j, 'が作れるか')
+                    print(a[:i+1], 'の中の整数のいくつかで', j, 'が作れるか')
                     print(dp)
+    print('===============================================')
+    print(a[:i+1], 'の中の整数のいくつかで', j, 'が作れるか')
     if dp[n][w]:
         print('Yes')
     else:
